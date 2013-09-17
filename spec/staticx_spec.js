b6491578d451904a89cf724a2e05eacc44523360
Richard Willis <willis.rh@gmail.com>
2013-09-17 20:07:59 +0100

    Added feature to compile markdown files from a filepath

--- spec/staticx_spec.js
+++ spec/staticx_spec.js
@@ -4,23 +4,46 @@ var staticx = require('../lib/staticx.js');
 
 describe('Generating HTML files', function() {
 
-  it('Compiles markdown files', function() {
+  describe('Compiling markdown', function() {
 
-    var compiler = staticx.Compiler('markdown');
-    var text = 'some *text* here';
-    var compiled;
+    var compiler = staticx.compiler.markdown;
 
-    compiler.compile(text, function(err, data) {
-      if (err) throw err;
-      compiled = data;
+    it('Compiles markdown files from a string of text.', function() {
+
+      var text = 'some *text* here';
+      var compiled;
+
+      compiler.compile(text, function(err, data) {
+        if (err) throw err;
+        compiled = data;
+      });
+
+      waitsFor(function() {
+        return compiled !== undefined;
+      }, 'Compilation took too long', 5000);
+
+      runs(function () {
+        expect(compiled.trim()).toBe('<p>some <em>text</em> here</p>');
+      });
     });
 
-    waitsFor(function() {
-      return compiled !== undefined;
-    }, 'Compilation took too long', 5000);
+    it('Compiles markdown files from a file path.', function() {
 
-    runs(function () {
-      expect(compiled.trim()).toBe('<p>some <em>text</em> here</p>');
+      var path = 'spec/fixtures/compiler_file_path.md';
+      var compiled;
+
+      compiler.compilePath(path, function(err, data) {
+        if (err) throw err;
+        compiled = data;
+      });
+
+      waitsFor(function() {
+        return compiled !== undefined;
+      }, 'Compilation took too long', 5000);
+
+      runs(function () {
+        expect(compiled.trim()).toBe('<p>some <em>text</em> here</p>');
+      });
     });
   });
 });
