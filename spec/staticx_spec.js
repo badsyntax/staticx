c1d8a72d46400e916c844009b02600de12c0c850
Richard Willis <willis.rh@gmail.com>
2013-09-17 19:48:06 +0100

    Got markdown compiler working using the marked module

--- spec/staticx_spec.js
+++ spec/staticx_spec.js
@@ -8,12 +8,19 @@ describe('Generating HTML files', function() {
 
     var compiler = staticx.Compiler('markdown');
     var text = 'some *text* here';
+    var compiled;
 
     compiler.compile(text, function(err, data) {
-
       if (err) throw err;
+      compiled = data;
+    });
 
-      expect(data).toBe('<p>some <em>text</em> here</p>');
+    waitsFor(function() {
+      return compiled !== undefined;
+    }, 'Compilation took too long', 5000);
+
+    runs(function () {
+      expect(compiled.trim()).toBe('<p>some <em>text</em> here</p>');
     });
   });
 });
