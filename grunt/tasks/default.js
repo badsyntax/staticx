bd708c761273c0a9ed75b504d4dd1956a15ef75b
Richard Willis <willis.rh@gmail.com>
2013-11-01 18:28:23 +0000

    Default task should lint because that task is performed by the test task

--- grunt/tasks/default.js
+++ grunt/tasks/default.js
@@ -1,8 +1,7 @@
 'use strict';
 
 module.exports = function(grunt) {
-	grunt.registerTask('default', 'The default grunt task.', [
-    'lint',
+  grunt.registerTask('default', 'The default grunt task.', [
     'test'
   ]);
 };
