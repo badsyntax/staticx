fae19b33b137f8113788dcb5d40c409b7852689d
Richard Willis <willis.rh@gmail.com>
2013-11-07 18:44:08 +0000

    First attempt at saving pages with parents

--- spec/staticx/models/page_spec.js
+++ spec/staticx/models/page_spec.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var fs = require('fs');
+var path = require('path');
+var async = require('async');
 var staticx = require('../../../lib/staticx.js');
 var Globalize = require('globalize');
 var PageModel = staticx.models.Page;
@@ -38,8 +40,9 @@ describe('Page Model', function() {
 
     var complete = false;
 
-    model.save(function(err) {
+    model.save(function(err, data) {
       if (err) throw err;
+      expect(data).toBe(model);
       markdownParser.parseFile(model.filePath, function(err, data) {
         if (err) throw err;
         complete = data;
@@ -61,6 +64,90 @@ describe('Page Model', function() {
       if (err) throw err;
       fs.exists(model.filePath, function(exists) {
         done(!exists ? null : 'Page still exists on filesystem');
+      });
+    });
+  });
+
+  it('Should validate the parent page correctly', function(done) {
+
+    // Check that an invalid parent page is validated.
+    var model1 = new TestModel({
+      title: 'test',
+      date: new Date(),
+      parent: 'doesnotexist'
+    });
+    var error = model1.validate();
+    expect(error.key).toBe('parent');
+
+    // Create the parent page.
+    new TestModel({
+      title: 'test hello',
+      date: new Date(),
+    }).save(function(err, parentModel) {
+      if (err) throw err;
+
+      // Check the valid parent page passes validation.
+      var model2 = new TestModel({
+        title: 'test',
+        date: new Date(),
+        parent: path.basename(parentModel.filePath, '.' + parentModel.fileExtension)
+      });
+      var error2 = model2.validate();
+      expect(error2).toBe(undefined);
+      parentModel.delete(done);
+    });
+  });
+
+  it('Should save the page data to file when specifying the parent page', function(done) {
+    // Create the first level page.
+    new TestModel({
+      title: 'first level',
+      date: new Date(),
+    }).save(function(err, firstModel) {
+      if (err) throw err;
+
+      var parent = path.basename(firstModel.filePath, '.' + firstModel.fileExtension);
+
+      // Create the second level page.
+      new TestModel({
+        title: 'second level',
+        date: new Date(),
+        parent: parent
+      }).save(function(err, secondModel) {
+        if (err) throw err;
+
+        var parent = [
+          secondModel.parent || '',
+          path.basename(secondModel.filePath, '.' + secondModel.fileExtension)
+        ].join('/');
+
+        var page = new TestModel({
+          title: 'third level',
+          date: new Date(),
+          parent: parent
+        });
+
+        page.save(function(err) {
+          if (err) throw err;
+          fs.exists(path.join(secondModel.destination, parent), function(exists) {
+            if (!exists) return done('Parent page directory does not exist');
+            fs.exists(page.filePath, function(exists) {
+              if (!exists) return done('Page not saved to file');
+              async.series([
+                firstModel.delete.bind(firstModel),
+                secondModel.delete.bind(secondModel),
+                page.delete.bind(page)
+              ], function(err) {
+                if (err) throw err;
+
+                /** TODO */
+                // We should check that empty directories are removed.
+
+                done(err);
+              });
+            });
+          });
+        });
       });
     });
   });
