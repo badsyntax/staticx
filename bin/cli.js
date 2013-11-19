e4707db29c4b042c255dc71b22d6435b3f08d2c5
Richard Willis <willis.rh@gmail.com>
2013-11-19 19:31:12 +0000

    Moved CLI object into seperate file. Bin file is now proxy to cli.js

---
+++ bin/cli.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+/*
+ * staticx
+ * https://github.com/badsyntax/staticx
+ *
+ * Copyright (c) 2013 Richard Willis
+ * Licensed under the MIT license.
+ */
+'use strict';
+
+/**
+ * Load dependencies.
+ */
+var program = require('commander');
+var pkg = require('../package.json');
+var AddPage = require('./commands/addpage');
+var Create = require('./commands/create');
+
+/**
+ * Init the CLI. Set the commands and options.
+ */
+exports.init = function() {
+
+  program.version(pkg.version);
+
+  program
+  .command('create')
+  .description('create a new site')
+  .option('--destination <destination>', 'destination path')
+  .option('--posts <posts>', 'number of dummy blog posts to generate', parseInt)
+  .option('--clean', 'clean the destination folder before creating')
+  .option('-i, --interactive', 'use prompts to set the options')
+  .action(Create.init.bind(Create));
+
+  program
+  .command('addpage')
+  .description('add a new page')
+  .option('--destination <destination>', 'destination path')
+  .option('--title <title>', 'page title')
+  .option('--parent <parent>', 'parent page')
+  .option('--date <date>', 'page date')
+  .option('--tags <tags>', 'page tags')
+  .option('-i, --interactive', 'use prompts to set the options')
+  .action(AddPage.init.bind(AddPage));
+
+  program.parse(process.argv);
+};
