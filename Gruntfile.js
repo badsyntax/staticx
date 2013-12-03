56e05dd5ad07d83de9e22b3ef27effd525b07e56
Richard Willis <willis.rh@gmail.com>
2013-12-03 14:07:38 +0000

    Catch uncaught exceptions thrown in async handlers

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,13 @@
 
 module.exports = function(grunt) {
 
+  // Catch unhandled exceptions and show the stack trace. This is most
+  // useful when running the jasmine specs.
+  process.on('uncaughtException',function(e) {
+    grunt.log.error('Caught unhandled exception: ' + e.toString());
+    grunt.log.error(e.stack);
+  });
+
   // Set the config.
   grunt.initConfig(require('./grunt/config'));
 
